ab46fd9c4f6f4b551e36facb1e29164023b2217f
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2025-03-16 10:43:18 +0200

    update index route to include logo

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,23 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({
-  component: Index,
-})
+export const Route = createFileRoute("/")({
+	component: Index,
+	loader: () => {
+		// Uncomment this to automatically redirect to a specific account
+		// throw redirect({
+		//   to: '/:accountId/inbox',
+		//   search: { tab: 'activity' },
+		//   params: { accountId: '9018283645' }
+		// })
+	},
+});
 
-function Index() {
-  return (
-    <div className="bg-[##FFFFFF]">
-      <h3>Welcome Home!</h3>
-    </div>
-  )
+export default function Index() {
+	return (
+		<div className="flex h-screen w-full items-center justify-center bg-[#18181B]">
+			<a href="/9018283645/inbox?tab=activity">
+				<img src="/logo.svg" alt="Logo" className="w-52 h-52" />
+			</a>
+		</div>
+	);
 }
